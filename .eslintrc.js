39c716f7cdd49feee914a72fc30c2f82045100ad
jungjunhyung99 <55770796+jungjunhyung99@users.noreply.github.com>
2023-12-20 11:23:26 +0900

    eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react/recommended",
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [".eslintrc.{js,cjs}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint", "react"],
+	rules: {
+		"react/react-in-jsx-scope": 0,
+		"react/prefer-stateless-function": 0,
+		"react/jsx-filename-extension": 0,
+		"react/jsx-one-expression-per-line": 0,
+		"no-nested-ternary": 0,
+		"react/function-component-definition": [
+			2,
+			{
+				namedComponents: "arrow-function",
+				unnamedComponents: "arrow-function",
+			},
+		],
+	},
+};
